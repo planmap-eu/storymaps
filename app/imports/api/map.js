1e9296226543b3f67a559309010e53d99566e27e
Carlos H Brandt <c.brandt@jacobs-university.de>
2019-10-08 19:01:40 +0200

    [wip] Structured map-container update

--- app/imports/api/map.js
+++ app/imports/api/map.js
@@ -1,31 +1,32 @@
-var default_maps = {
-    opm_mars: {
-        url: "http://s3-eu-west-1.amazonaws.com/whereonmars.cartodb.net/mola-gray/{z}/{x}/{y}.png",
-        options: {
-            crs: L.CRS.EPSG3857,
-            maxNativeZoom: 9,
-            tms: true,
-            autoZIndex: true,
-            attribution: "<a href='https://github.com/openplanetary/opm/wiki/OPM-Basemaps' target='_blank'>OpenPlanetaryMap</a>"
-        },
-    },
-    opm_moon: {
-        url: "https://s3.amazonaws.com/opmbuilder/301_moon/tiles/w/hillshaded-albedo/{z}/{x}/{y}.png",
-        options: {
-            crs: L.CRS.EPSG3857,
-            maxNativeZoom: 9,
-            tms: false,
-            autoZIndex: true,
-            attribution: "<a href='https://astrogeology.usgs.gov/search/map/Moon/LMMP/LOLA-derived/Lunar_LRO_LOLA_Shade_Global_128ppd_v04' target='_blank'>LOLA/USGS</a>"
-        },
-    }
-}
+// var default_maps = {
+//     opm_mars: {
+//         url: "http://s3-eu-west-1.amazonaws.com/whereonmars.cartodb.net/mola-gray/{z}/{x}/{y}.png",
+//         options: {
+//             crs: L.CRS.EPSG3857,
+//             maxNativeZoom: 9,
+//             tms: true,
+//             autoZIndex: true,
+//             attribution: "<a href='https://github.com/openplanetary/opm/wiki/OPM-Basemaps' target='_blank'>OpenPlanetaryMap</a>"
+//         },
+//     },
+//     opm_moon: {
+//         url: "https://s3.amazonaws.com/opmbuilder/301_moon/tiles/w/hillshaded-albedo/{z}/{x}/{y}.png",
+//         options: {
+//             crs: L.CRS.EPSG3857,
+//             maxNativeZoom: 9,
+//             tms: false,
+//             autoZIndex: true,
+//             attribution: "<a href='https://astrogeology.usgs.gov/search/map/Moon/LMMP/LOLA-derived/Lunar_LRO_LOLA_Shade_Global_128ppd_v04' target='_blank'>LOLA/USGS</a>"
+//         },
+//     }
+// }
 
 
 class Map {
     constructor() {
         this._map = null;
         this._data = null;
+        this._overlay = null;
     }
 
     getMap() {
@@ -56,12 +57,12 @@ class Map {
                 [extent[2], extent[3]]
             ]);
         } else {
-            var center = map_settings.options.center;
+            var center = map_settings.center;
             if (!center) {
                 center = [0, 0];
             }
             console.log(`Map settings center: ${center}`);
-            map.setView(center, 2);
+            map.setView(center, 3);
         }
 
         L.tileLayer(map_settings.url, map_settings.options).addTo(map);
@@ -72,23 +73,62 @@ class Map {
     update(params) {
         if (params) {
             var map = this._map;
-            if (params.extent) {
-                var extent = params.extent;
-                map.fitBounds([
-                    [extent[0], extent[1]],
-                    [extent[2], extent[3]]
-                ]);
-            }
-            if (params.marker) {
-                var marker = params.marker;
-                L.marker(marker).addTo(map);
-                // .bindPopup('A marker.')
-                // .openPopup();
-            }
-            if (params.layer) {
-                // Add the layer. Layers can be GeoJSON, WMS or WFS.
+            if (map) {
+              var view = params.view;
+              if (view) {
+                  var extent = view.extent;
+                  console.log(`View extent: ${extent}`);
+                  if (extent) {
+                    map.flyToBounds([
+                        [extent[1], extent[0]],
+                        [extent[3], extent[2]]
+                    ]);
+                  }
+              }
+              var marker = params.marker;
+              if (marker) {
+                  L.marker(marker).addTo(map);
+                  // .bindPopup('A marker.')
+                  // .openPopup();
+              }
+              var layers = params.layers;
+              if (layers) {
+                this.setLayers(layers);
+              }
             }
         }
+    }
+
+    setLayers(layers) {
+      var overlay = new L.FeatureGroup();
+      layers.forEach(({path, credits, type}) => {
+        var layer;
+        if (type == 'tms') {
+          var url = 'http://localhost:8000/' + path + '/{z}/{x}/{y}.png';
+          console.log(url);
+          layer = new L.tileLayer(url,
+                                  {
+                                      tms: true,
+                                      opacity: 0.9,
+                                      attribution: credits
+                                  }
+                                  );
+        }
+        console.log(`Layer: ${layer}`);
+        if (!!layer) {
+          overlay.addLayer(layer);
+        }
+      });
+      console.log(`Overlay: ${overlay}`);
+      this.setOverlay(overlay);
+    }
+
+    setOverlay(overlay) {
+      if (this._overlay) {
+        this._map.removeLayer(this._overlay);
+      }
+      overlay.addTo(this._map);
+      this._overlay = overlay;
     }
 }
 
--- app/imports/ui/mapcontainer.js
+++ app/imports/ui/mapcontainer.js
@@ -8,6 +8,40 @@ import './mapcontainer.html';
 import { map } from '/imports/api/map.js';
 
 
+/* -------------------------------------------------------------------------
+Async function to get the story data; It is a call to the server.
+The map (leaflet) can only be built if/when the client gets the data right.
+*/
+const _buildMap = async (body,label) => {
+    await Meteor.call('getStory', {body,label}, (err, res) => {
+        if (err) {
+            console.log(`Something went wrong for story ${body},${label}`);
+        }
+        map.build(res.basemap, 'mapcontainer');
+    })
+}
+/* ------------------------------------------------------------------------- */
+
+Template.mapcontainer.onCreated(function() {
+  this.autorun(() => {
+        var chapter_index = Session.get('currentChapter');
+        var story_data = Session.get('currentData');
+//         console.log(`Chapter index: ${chapter_index}`);
+        if (chapter_index >= 0) {
+//             console.log(`Story data: ${JSON.stringify(story_data)}`);
+            if (story_data) {
+                var chapter_data = story_data.chapters[chapter_index];
+//                 console.log(`Chapter data: ${JSON.stringify(chapter_data)}`);
+                var view = chapter_data.view;
+                var marker = chapter_data.marker;
+                var layers = chapter_data.layers;
+                map.update({view, marker, layers});
+            }
+        }
+    })
+})
+
+
 Template.mapcontainer.onDestroyed(() => {
     map.clean();
 })
@@ -21,23 +55,14 @@ Template.mapcontainer.onRendered(() => {
     _buildMap(BODY,LABEL);
 })
 
-const _buildMap = async (body,label) => {
-    await Meteor.call('getStory', {body,label}, (err, res) => {
-        if (err) {
-            console.log(`Something went wrong for story ${body},${label}`);
-        }
-        map.build(res.basemap, 'mapcontainer');
-    })
-}
 
-
-Template.storypanel.helpers({
+Template.storymap.helpers({
     chapter: function() {
         var chapter_index = Session.get('currentChapter');
         var story_data = Session.get('currentData');
         console.log(`Chapter index: ${chapter_index}`);
         if (chapter_index >= 0) {
-            console.log(`Story data: ${JSON.stringify(story_data)}`);
+            // console.log(`Story data: ${JSON.stringify(story_data)}`);
             if (story_data) {
                 var chapter_data = story_data.chapters[chapter_index];
                 console.log(`Chapter data: ${JSON.stringify(chapter_data)}`);
@@ -46,8 +71,11 @@ Template.storypanel.helpers({
         } else {
           return null;
         }
-    },
+    }
+})
 
+
+Template.storypanel.helpers({
     many_items_in: function(array) {
       return array.length > 1;
     }
