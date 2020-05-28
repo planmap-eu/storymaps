1c7d85d6b279caeff7743283f7f0d0bf50b5cbec
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-05-28 21:07:17 +0200

    Add overlay and action from thumbnail to fullscreen

--- app/imports/ui/story_panel.js
+++ app/imports/ui/story_panel.js
@@ -6,27 +6,41 @@ Template.storypanel.helpers({
     }
 })
 
-
 Template.mediacanvas.helpers({
     setCanvas(type, url, width) {
         var hws = ["src='"+url+"'"];
         var element;
         if (type == 'image/png' || type == 'image/jpeg' || type == 'image/jpg') {
             element = "<img style='width:100%' "+hws+">";
-            return element;
         } else {
             if (type == 'video/youtube') {
-                // return '<div style="width:100px"><iframe '+hws+' frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"></iframe></div>';
-                return '<div class="video-container"><iframe '+hws+' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="videoiframe"></iframe></div>';
+                element = '<div class="video-container"><iframe '+hws+' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="videoiframe"></iframe></div>';
             } else {
-
-                if (type == 'model/gltf-binary') {
-                    console.log("Not working with 3D models yet.")
-                    return type;
+                if (type == '3d/sketchfab') {
+                  element = '<div class="video-container"><iframe '+hws+' title="A 3D model" width="400" height="300" frameborder="0" allow="fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe></div>';
                 }
+                // if (type == 'model/gltf-binary') {
+                //     console.log("Not working with 3D models yet.")
+                //     return type;
+                // }
             }
         }
-        console.log("I have no idea how to handle " + type);
-        return type;
+        if (!element) {
+          console.log("I have no idea how to handle " + type);
+          return null;
+        }
+        return element;
+    }
+})
+
+Template.mediacanvas.events({
+    'click a.toOverlay' (event,instance) {
+        event.preventDefault();
+        var overlay_content = document.getElementById("overlay-content");
+        // overlay_content.innerHTML = event.currentTarget.innerHTML;
+        var media_div = document.getElementById(event.currentTarget.dataset.id_);
+        overlay_content.innerHTML = media_div.innerHTML;
+        var overlay = document.getElementById("overlay");
+        overlay.style.display = "table-cell";
     }
 })
